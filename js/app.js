62d2fe55c490184103e079b8c8a306caf8afb58a
chenoaPinto <chenodelvalle@gmail.com>
2021-09-08 20:22:13 -0300

    clase de repaso

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//alert()//
+let resultado=0;/**variable con scope global*/
+/*String, int , bool, undefine,array object, null*/
+console.log(suma());
+console.log(resultado);
+function suma(){
+    resultado = 10; 
+    return resultado;
+}
+let nombre = "Luis";
+let apellido = "Navas";
+let edad = 48;
+let llueve = true;
+let participantes=[
+    "Junior", "Luis", "Ivana", 
+    "Martin", "Sheila", "Chenoa", 
+    "Marcos","Sebastian"];
+for(let a=0; a < participantes.length; a++){
+    console.log(participantes[a]);
+}
+
+function render(){
+    let contenedor = document.getElementById('root');
+    participantes.forEach(part=>contenedor.innerHTML+=
+        `<div class='card col-4'>
+            <img src='...' class='card-img-top' alt='...'>
+            <div class='card-body'>
+                <h5 class='card-title'>${part}</h5>
+                <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href='#' class='btn-primary'>Go somewhere</a>
+            </div>
+        </div>` 
+        );
+}
+render();
